71e60442d3c07a0599decb9303f27333bba41292
walrusfruitcake <walrusfruitcake@users.noreply.github.com>
2020-07-09 21:03:11 -0400

    WIP: working on reading from the rom
    Signed-off-by: walterscarborough <walterscarborough@users.noreply.github.com>

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -1,13 +1,22 @@
+import '../support/commands'
+
 context('emulator index page', () => {
     it('renders the welcome screen', () => {
         cy.visit('http://localhost:8081');
 
         cy.get('[data-load-file]').should('exist');
     });
+
     it('Renders the flag on screen', () => {
         cy.visit('http://localhost:8081');
+
+        cy.get('[data-flags]').contains('Carry').should('not.exist');
+
+        cy.fixture('clear-flag.a26').as('game')
+
+        const gamePath = '../fixtures/clear-flag.a26';
+        cy.get('[data-flags]').attachFile(gamePath);
 
         cy.get('[data-flags]').contains('Carry');
     });
 });
-
--- src/ts/__tests__/index.test.ts
+++ src/ts/__tests__/index.test.ts
@@ -4,10 +4,18 @@ describe('Smoke Test', () => {
   });
 });
 
-describe('', () => {
-  it('should read an instruction from the ROM', () => {
-  });
-  it('should do what the instruction from the ROM says', () => {
+describe('someHandler', () => {
 
+  // once upload is complete, pass rom into the cpu
+  it('should read an instruction from the ROM', () => {
+
+  });
+
+  // read data from the rom file in a loop
+
+  it('should do what the instruction from the ROM says', () => {
+      // what does the html 5 file api provide as an input to other things
+
+      someHandler()
   });
 });
